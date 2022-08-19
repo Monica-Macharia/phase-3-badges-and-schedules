7500eb518fc2e75bfb887aa89fd8fec1d69510d3
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-08-19 16:36:17 +0300

    commit tree f4d99c5e778995b5282deda4c9de07a78a15a1de

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    array.map {|item| badge_maker(item)}
+    
+end
+def assign_rooms(array)
+            array.map.with_index(1) do |name, num| 
+           "Hello, #{name}! You'll be assigned to room #{num}!"  
+    end
+end 
+
+def printer(array)
+    batch_badge_creator(array).each do |b|
+        puts b
+    end
+    assign_rooms(array).each do |num|
+        puts num
+end
+end
